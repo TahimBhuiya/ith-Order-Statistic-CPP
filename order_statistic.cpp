a39d338c503e00b7f4b67cc1da210ac2e2f3c5e0
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-11-02 17:05:08 -0500

    Added comments

--- order_statistic.cpp
+++ order_statistic.cpp
@@ -3,4 +3,11 @@
 #include <iostream> //Added necessary libraries
 using namespace std; 
 
-int partition(int arr[], int low, int high) {}
+// Partition function: rearranges elements around a pivot
+int partition(int arr[], int low, int high) {
+
+
+
+
+
+}
