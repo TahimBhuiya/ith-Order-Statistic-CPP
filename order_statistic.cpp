6295acd8ba4bdd2e6439b34951bb935455f9acdf
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-11-02 17:06:23 -0500

    Added comments

--- order_statistic.cpp
+++ order_statistic.cpp
@@ -8,7 +8,10 @@ int partition(int arr[], int low, int high) {
     int pivot = arr[high];  // choose the last element as pivot
     int i = low - 1;        // index of smaller element
 
-    
+    for (int j = low; j < high; j++) {
+        
+        
+    }
 
 
 }
