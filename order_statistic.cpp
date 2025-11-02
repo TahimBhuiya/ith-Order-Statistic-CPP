a1901cfbb38d46ec90ed1a7038738dfe7e002036
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-11-02 17:06:04 -0500

    Added onto Partition function

--- order_statistic.cpp
+++ order_statistic.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 // Partition function: rearranges elements around a pivot
 int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  
+           
 
-
-
+    
 
 
 }
